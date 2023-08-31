df679b47a31b65af0870f4a281523f45bcb0271a
FopGen <fopgen@gmail.com>
2023-08-31 08:23:38 +0300

    Init commit

---
+++ src/FirstClassForGit.java
@@ -0,0 +1,12 @@
+public class FirstClassForGit {
+    public static void main(String[] args) {
+        int x = 3;
+        System.out.println(sum(x));
+    }
+    public static int sum(int x){
+           x+=x;
+           return x;
+    }
+  //  System.out.println(x);
+
+}
